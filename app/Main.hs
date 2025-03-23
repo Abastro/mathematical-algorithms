e0c0af7cd2bf7c0db8b0a26b118cdeb022ea60a1
Abastro <abab9579@gmail.com>
2025-03-23 16:56:30 +0900

    Polynomial stuff

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,13 @@
-module Main where
+module Main (main) where
+
+import Data.Vector qualified as V
+import Poly
 
 main :: IO ()
 main = do
-  putStrLn "Hello, Haskell!"
-
+  let f :: Poly Int = Poly (V.fromList [1, 2, 3])
+  let g :: Poly Int = Poly (V.fromList [4, 5])
+  putStrLn $ "f: " <> show f <> ", g: " <> show g
+  putStrLn $ "f + g: " <> show (f + g)
+  putStrLn $ "f * g: " <> show (f * g)
+  putStrLn $ "Karatsuba f * g: " <> show (normalize $ karatsubaMult f g)
